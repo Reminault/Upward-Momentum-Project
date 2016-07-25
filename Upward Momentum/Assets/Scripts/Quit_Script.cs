80257523079830e042b9f7ec98bf5a4c4ab2dc33
Reminault <nilemc12@hotmail.com>
2016-07-25 14:08:09 +0400

    Folder Changes

---
+++ Upward Momentum/Assets/Scripts/Quit_Script.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Quit_Script : MonoBehaviour {
+
+    public Button quitButton;
+
+        void Start () 
+        {
+
+            quitButton.onClick.AddListener(() =>
+            {
+                Application.Quit();
+            });
+        }
+}
